b66c9351114745a902278d6c267aecfce0757956
Kristiyan-Petkov <kristiyanp@gmail.com>
2022-08-06 23:41:10 +0200

    Using props attributes

--- 01_Intro to React and JSX/create-react-app-demo-notes/src/Header.js
+++ 01_Intro to React and JSX/create-react-app-demo-notes/src/Header.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
-export default function Header() {
-    return <header className="App-header">
-        <h1>Welcome to React!</h1>
+export default function Header(props) {
+    return (<header className="App-header">
+        <h1>{props.text}</h1>
         <img src={logo} className="App-logo" alt="logo" />
-    </header>
+    </header>)
 };
