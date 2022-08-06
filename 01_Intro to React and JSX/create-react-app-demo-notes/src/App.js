274c67e70ba76c11d07e2d5f320b6045973be21b
Kristiyan-Petkov <kristiyanp@gmail.com>
2022-08-06 23:19:11 +0200

    Renaming and Header component separated

---
+++ 01_Intro to React and JSX/create-react-app-demo-notes/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import Footer from './Footer';
+import Header from './Header';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
