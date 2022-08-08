772855c2a65abb6d76431128b8010a37bf52c364
Kristiyan-Petkov <kristiyanp@gmail.com>
2022-08-08 15:45:44 +0200

    Split app in main components

---
+++ 00_react_exam_project/src/components/Promos.js
@@ -0,0 +1,56 @@
+export const Promos = () => {
+    return (<section className="hot-promos">
+        <h2>Lorem Ipsum</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        <ul>
+            <li>
+                <img src="./Images/photo-kids-playing-football.jpg" alt="kidsFootball" />
+                <section className="info">
+                    <div>
+                        <h3>Lorem Ipsum</h3>
+                        <p>15h February 2019</p>
+                    </div>
+                    <div>
+                        $35
+                    </div>
+                </section>
+                <section className="moreInfo">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    <button>ENROLL TODAY</button>
+                </section>
+            </li>
+            <li>
+                <img src="./Images/swimming-pool.jpeg" alt="swimming" />
+                <section className="info">
+                    <div>
+                        <h3>Lorem Ipsum</h3>
+                        <p>15h February 2019</p>
+                    </div>
+                    <div>
+                        $35
+                    </div>
+                </section>
+                <section className="moreInfo">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    <button>ENROLL TODAY</button>
+                </section>
+            </li>
+            <li>
+                <img src="./Images/kids_dogs_playing.jpg" alt="kidsFootball" />
+                <section className="info">
+                    <div>
+                        <h3>Dog Day</h3>
+                        <p>15h February 2019</p>
+                    </div>
+                    <div>
+                        $35
+                    </div>
+                </section>
+                <section className="moreInfo">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                    <button>ENROLL TODAY</button>
+                </section>
+            </li>
+        </ul>
+    </section>)
+}
