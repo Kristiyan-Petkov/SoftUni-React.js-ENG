88f6ac11b49cb6abb2458a238d142624142def19
Kristiyan-Petkov <kristiyanp@gmail.com>
2022-08-13 15:04:14 +0200

    Dynamin rendering of users from db

--- 04_ReactJS_Components_Workshop/client/src/App.js
+++ 04_ReactJS_Components_Workshop/client/src/App.js
@@ -1,18 +1,33 @@
 import { Header } from "./components/common/Header";
 import { Footer } from "./components/common/Footer";
 import { Search } from "./components/search/Search";
-import { UserSection } from "./components/user-section/UserSection";
+import { UserList } from "./components/user-list/UserList";
 import './App.css';
+import {useState, useEffect} from "react";
+
+const baseUrl = "http://localhost:3005/api";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${baseUrl}/users`)
+      .then(res => res.json())
+      .then(result => {
+        setUsers(result.users)
+      })
+  }, []);
+
+  // console.log(users);
+
   return (
     <div className="App">
       <Header />
       <main className="main">
         {/* <!-- Section component  --> */}
-        <section class="card users-container">
+        <section className="card users-container">
           <Search />
-          <UserSection />
+          <UserList users={users}/>
         </section>
       </main>
       <Footer />
--- 04_ReactJS_Components_Workshop/client/src/components/common/Footer.js
+++ 04_ReactJS_Components_Workshop/client/src/components/common/Footer.js
@@ -1,5 +1,5 @@
 export const Footer = () => {
-    return (<footer class="footer">
+    return (<footer className="footer">
         <p>Copyright &copy; designed by Mihail Valkov</p>
     </footer>)
 }
